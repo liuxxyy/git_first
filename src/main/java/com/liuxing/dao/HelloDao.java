508a0978794a2f08d9c3ce6aecb832030850b69f
liux <lx95728@163.com>
2018-10-09 16:44:16 +0800

    First

---
+++ src/main/java/com/liuxing/dao/HelloDao.java
@@ -0,0 +1,12 @@
+package com.liuxing.dao;
+
+/**
+ * @ Author     ：LiuX.
+ * @ Date       ：Created in  18/10/9/009 16:41
+ * @ Modified By：
+ */
+public class HelloDao {
+    public static void main(String[] args) {
+        System.out.println("今天是个好日子啊@@@@");
+    }
+}
